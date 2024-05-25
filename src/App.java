480603fa650d62af5b867138bad5fd4ca728659d
kk <kazmerchuk19@gmail.com>
2024-05-25 16:39:11 +0300

    + Katya vetca

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
+// + vetca
 public class App extends Application{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
